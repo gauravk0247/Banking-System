58736da5c030e941ed124acb60b55e750410fcf3
gauravk0247 <86191685+gauravk0247@users.noreply.github.com>
2022-02-08 14:53:28 +0530

    Add files via upload

---
+++ cuser.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/cuser.css">
+    <link rel="stylesheet" href="css/table.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <title>Spark - Bank</title>
+    <style>
+        #hei{
+            min-height: 537px;
+        }
+    </style>
+</head>
+
+<body style="background-color : #ececec;">
+    <?php include 'partials/_header.php'; ?>
+    <?php include 'partials/_dbconnect.php'; ?>
+
+    <?php
+    include 'partials/_dbconnect.php';
+    if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $gender=$_POST['gender'];
+    $balance=$_POST['balance'];
+    $sql="insert into users(name,email,gender,balance) values('{$name}','{$email}','{$gender}','{$balance}')";
+    $result=mysqli_query($conn,$sql);
+    if($result){
+               echo "<script> alert('User has been created!');
+                               window.location='transfer.php';
+                     </script>";
+                    
+    }
+  }
+?>
+
+
+    <h2 class="text-center pt-2" style="color : #6c757d;">Create a User</h2>
+    <br>
+
+    <div class="background mb-4" id="hei">
+        <div class="container">
+            <div class="screen">
+                <div class="screen-header">
+                    <div class="screen-header-right">
+                        <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                    </div>
+                </div>
+                <div class="screen-body">
+                    <div class="screen-body-item left">
+                        <img class="img-fluid" src="img/cuser.png" style="border: none; border-radius: 10%;">
+                    </div>
+                    <div class="screen-body-item">
+                        <form class="app-form" method="post">
+                            <div class="app-form-group">
+                                <input class="app-form-control" placeholder="FULLNAME" type="text" name="name" required>
+                            </div>
+                            <div class="app-form-group">
+                                <input class="app-form-control" placeholder="EMAIL" type="email" name="email" required>
+                            </div>
+                            <div class="app-form-group">
+                                <select name="gender" class="app-form-control" type="text" required>
+                                    <option selected>SELECT</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                            </div>
+                            <div class="app-form-group">
+                                <input class="app-form-control" placeholder="INITIAL BALANCE" type="number"
+                                    name="balance" required>
+                            </div>
+                            <br>
+                            <div class="app-form-group button">
+                                <input class="app-form-button" style="color:#4d865a;" type="submit" value="CREATE USER"
+                                    name="submit"></input>
+                                <input class="app-form-button" style="color:#dc3545;" type="reset" value="RESET"
+                                    name="reset"></input>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'partials/_footer.php'; ?>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
